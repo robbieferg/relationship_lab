ca2f392106af30d1623275e3afa5d2fff31827d4
Robbie Ferguson <robbiejferguson92@gmail.com>
2022-02-15 10:45:59 +0000

    Initial commit

---
+++ src/test/java/com/codeclan/example/relationships_lab/RelationshipsLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.relationships_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationshipsLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
